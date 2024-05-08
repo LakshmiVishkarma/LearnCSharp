e0ac743cdc0b31b5524ec76b9bbdb9d0e4a336c8
Lakshmi Vishkarma <85280785+LakshmiVishkarma@users.noreply.github.com>
2024-05-08 07:44:05 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using learncSharp_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace learncSharp_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Topics()
+        {
+            CodeVM codeVM = new CodeVM();
+            return View(codeVM);
+        }
+
+        [HttpPost]
+        public IActionResult Topics(CodeVM codeVM)
+        {
+            return View(codeVM);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CodeVM.cs
@@ -0,0 +1,7 @@
+﻿namespace learncSharp_Website.Models
+{
+    public class CodeVM
+    {
+        public string text { get; set; } = "Hello World!";
+    }
+}
